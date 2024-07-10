328f80f4875ed2f8f37625c81daf5c2b00b7cd03
Om Patel <omunite21@gmail.com>
2024-07-10 13:24:56 +0530

    Building Registration Page

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -1,10 +1,18 @@
+import type { CreateUserParams } from "@/types";
+import { users } from "../appwrite.config";
+import { ID, Query } from "node-appwrite";
+
 export const createUser = async (user: CreateUserParams) => { 
 try{
-
+    const newUser = await users.create(ID.unique(), user.email, user.phone, undefined, user.name)
 }
-catch(error){
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+catch(error: any){
     if(error && error?.code === 409) {
-        
+        const documents = await users.list([
+            Query.equal('email', [user.email])
+        ])
+        return documents?.users[0]
     }
 }
 }
